1c2809244d511b0f0cd6a1c3c9fc12183b5981d3
Ghosuto <clash.raja10@gmail.com>
2025-10-21 10:02:26 +0000

    Init empty extra repo
    for extras and addons repos

---
+++ config.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) LightningOS 2026
+#               
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+$(call inherit-product, vendor/extras/themes/bootanimation/bootanimation.mk)
